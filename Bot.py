b3f665283672a2e55036da6679ae0643beb5b51d
Dashka_Smile <125667312+darshuta@users.noreply.github.com>
2023-07-18 21:23:10 +0300

    Используются новые функции, добавлены новые команды

--- Bot.py
+++ Bot.py
@@ -1,4 +1,7 @@
 import discord
+import random
+import requests
+import os
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -45,7 +48,54 @@ async def help_(ctx):
                    '$hello - приветствие бота\n'
                    '$add <два произвольных числа> - сложение двух чисел\n'
                    '$heh <количество повторов> - вывод размноженного слова "heh"\n'
-                   '$revers <произвольное слово> - вывод слова в обратной последовательности')
+                   '$revers <произвольное слово> - вывод слова в обратной последовательности'
+                   '$duck - вывод картинки с уткой'
+                   '$Deck_of_Cards - вывод игральной карты'
+                   '$mem - вывод картинки с мемом')
+
+
+def get_duck_image_url():
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+def get_Deck_of_Cards_image_url():
+    """Функция возвращает произвольную карту из колоды карт"""
+
+    params = {'count': '1'}
+    res = requests.get('https://deckofcardsapi.com/api/deck/new/draw', params=params)
+
+    data = res.json()
+
+    return data['cards'][0]['image']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    """По команде duck вызывает функцию get_duck_image_url"""
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+
+@bot.command('Deck_of_Cards')
+async def Deck_of_Cards(ctx):
+    """По команде Deck_of_Cards вызывается функция get_Deck_of_Cards_image_url, возвращающая произвольную карта из колоды карт"""
+    image_url = get_Deck_of_Cards_image_url()
+    await ctx.send(image_url)
+
+
+@bot.command()
+async def mem(ctx):
+    lst = os.listdir('images')
+    img_name = random.choice(lst)
+    with open(f'images/{img_name}', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+
+    # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
 
 
 bot.run("ТУТ УКАЖИТЕ ТОКЕН СВОЕГО БОТА")
